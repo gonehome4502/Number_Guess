6afbb22b8325d6cdcbf2145cf31ccf669c75e098
Travis <155777858+gonehome4502@users.noreply.github.com>
2024-02-21 13:37:57 -0600

    Create number_guess.sql

---
+++ number_guess.sql
@@ -0,0 +1,139 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+-- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE guessing_game;
+--
+-- Name: guessing_game; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE guessing_game WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE guessing_game OWNER TO freecodecamp;
+
+\connect guessing_game
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.users (
+    user_id integer NOT NULL,
+    username character varying(22),
+    games_played integer DEFAULT 0 NOT NULL,
+    best_game integer
+);
+
+
+ALTER TABLE public.users OWNER TO freecodecamp;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_user_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.users VALUES (21, 'user_1708543044762', 2, 343);
+INSERT INTO public.users VALUES (20, 'user_1708543044763', 5, 77);
+INSERT INTO public.users VALUES (33, 'user_1708543937623', 2, 129);
+INSERT INTO public.users VALUES (32, 'user_1708543937624', 5, 317);
+INSERT INTO public.users VALUES (23, 'user_1708543096196', 2, 402);
+INSERT INTO public.users VALUES (22, 'user_1708543096197', 5, 197);
+INSERT INTO public.users VALUES (35, 'user_1708544038571', 2, 240);
+INSERT INTO public.users VALUES (25, 'user_1708543123701', 2, 635);
+INSERT INTO public.users VALUES (34, 'user_1708544038572', 5, 128);
+INSERT INTO public.users VALUES (24, 'user_1708543123702', 5, 200);
+INSERT INTO public.users VALUES (27, 'user_1708543220771', 2, 254);
+INSERT INTO public.users VALUES (26, 'user_1708543220772', 5, 245);
+INSERT INTO public.users VALUES (37, 'user_1708544096285', 2, 157);
+INSERT INTO public.users VALUES (36, 'user_1708544096286', 5, 305);
+INSERT INTO public.users VALUES (29, 'user_1708543395044', 2, 272);
+INSERT INTO public.users VALUES (28, 'user_1708543395045', 5, 245);
+INSERT INTO public.users VALUES (31, 'user_1708543411061', 2, 543);
+INSERT INTO public.users VALUES (30, 'user_1708543411063', 5, 2);
+
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.users_user_id_seq', 37, true);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_username_key UNIQUE (username);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
